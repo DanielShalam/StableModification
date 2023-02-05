92a3e099e79ed3cd7bc1d20a974112324a3249c8
DaniDush <dani360@gmail.com>
2023-02-05 11:16:14 +0200

    Adding prompt2prompt and null inversion

---
+++ prompt2prompt.py
@@ -0,0 +1,112 @@
+import os
+import argparse
+import numpy as np
+import torch
+from omegaconf import OmegaConf
+from pytorch_lightning import seed_everything
+
+import utils
+import ptp_utils
+from null_inversion import NullInversion
+
+MODEL = None
+CONTEXT = None
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config", type=str,
+        default="./configs/prompt2prompt.yaml"
+    )
+    parser.add_argument("-w", "--weights", type=float, nargs='+', default=[-1, 2, 4, 8])
+    parser.add_argument("--outdir", type=str, default="./outputs/prompt2prompt/", )
+    parser.add_argument("--seed", type=int, default=43)
+    parser.add_argument("--repeat", type=int, help="how many times to repeat the experiment with different seed.",
+                        default=1, )
+    opt = parser.parse_args()
+    return opt
+
+
+def get_prompts(exp_cfg):
+    """
+    Parsing config file
+    """
+    prompts = [exp_cfg["prompt_init"]] + list(exp_cfg["prompt_new"])
+    return prompts
+
+
+def get_source_img(exp_cfg, model, generator, prompt_src):
+    images_save = []
+    # generating initial image
+    if exp_cfg.img_path and exp_cfg.img_path != "":
+        print("Perform Null text inversion")
+        null_inversion = NullInversion(model, steps=exp_cfg.steps, guidance_scale=exp_cfg.scale)
+        (image_gt, imgs_src), x_t, uncond_embeddings = null_inversion.invert(exp_cfg.img_path, prompt_src,
+                                                                             offsets=(0, 0, 200, 0),
+                                                                             verbose=True)
+        images_save.append(ptp_utils.text_under_image(image_gt, text="source image"))
+        images_save.append(ptp_utils.text_under_image(imgs_src.copy(), text="reconstructed image (inverted)"))
+    else:
+        print("Generate source image")
+        uncond_embeddings = None
+        controller = ptp_utils.AttentionStore()
+        imgs_src, x_t = ptp_utils.text2image_ldm_stable(model, [prompt_src], controller, exp_cfg.steps,
+                                                        latent=None, guidance_scale=exp_cfg.scale,
+                                                        generator=generator)
+        images_save.append(ptp_utils.text_under_image(imgs_src[0].copy(), text="source image"))
+
+    return x_t, imgs_src, uncond_embeddings, images_save
+
+
+def main():
+    seed_everything(args.seed)
+    os.makedirs(args.outdir, exist_ok=True)
+    base_count = len(os.listdir(args.outdir)) + 1
+    model, tokenizer = utils.load_model_hugging(version=0)
+    print(args)
+
+    # load experiments from config file
+    config = OmegaConf.load(f"{args.config}")
+    experiments = config["experiments"]
+    imgs_src = None
+    uncond_embeddings = None
+    x_t = None
+
+    # iterate over experiments
+    for exp_idx, exp_key in enumerate(experiments.keys()):
+        exp_cfg = experiments[exp_key]
+        prompts = get_prompts(exp_cfg)
+
+        print(f"start experiment {exp_key} with prompts: ")
+        print("prompt source:", prompts[0], "\nprompt target: ", prompts[1], "\n")
+
+        for r in range(args.repeat):
+            g_cpu = torch.Generator().manual_seed(args.seed + r)
+            if exp_idx == 0 or not exp_cfg.resume_from_last:
+                x_t, imgs_src, uncond_embeddings, images_save = get_source_img(exp_cfg, model, g_cpu, prompts[0])
+            else:
+                images_save = [ptp_utils.text_under_image(imgs_src[0], text="source image")]
+
+            # iterate over list of weights
+            weights = [1] if not exp_cfg.reweight.apply else exp_cfg.reweight.weights
+            for weight in weights:
+                # get reweighing controller
+                controller = utils.get_controller(exp_cfg, tokenizer, prompts, (float(weight),))
+
+                # actual editing
+                images, _ = ptp_utils.text2image_ldm_stable(model, prompts, controller, exp_cfg.steps, exp_cfg.scale,
+                                                            latent=x_t.clone(), uncond_embeddings=uncond_embeddings)
+
+                image = images[1] if np.ndim(images) > 3 else images
+                images_text = ptp_utils.text_under_image(image, text=f"weight: {weight}")
+                images_save.append(images_text)
+
+            images_save_pil = ptp_utils.view_images(np.stack(images_save))
+            images_save_pil.save(os.path.join(args.outdir, f"{base_count:05}.png"))
+            base_count += 1
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main()
---
+++ utils.py
@@ -0,0 +1,129 @@
+import torch
+from omegaconf import OmegaConf
+from transformers import CLIPTextModel, CLIPTokenizer
+from diffusers import AutoencoderKL, UNet2DConditionModel, PNDMScheduler, LMSDiscreteScheduler,\
+    StableDiffusionPipeline, DiffusionPipeline, DDIMScheduler
+
+import ptp_utils
+from ldm.util import instantiate_from_config
+
+API_TOKEN = "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"
+model_ids = ["CompVis/stable-diffusion-v1-4", "stabilityai/stable-diffusion-2"]
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+def get_v2_modules():
+    # 1. Load the autoencoder model which will be used to decode the latents into image space.
+    vae = AutoencoderKL.from_pretrained(model_ids[1], subfolder="vae").to(device)
+
+    # 2. Load the tokenizer and text encoder to tokenize and encode the text.
+    tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+    text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14").to(device)
+
+    # 3. The UNet model for generating the latents.
+    unet = UNet2DConditionModel.from_pretrained(model_ids[1], subfolder="unet").to(device)
+    return vae, tokenizer, text_encoder, unet
+
+
+def load_model_hugging(version: int = 0):
+    """
+    Load pretrained diffusion model from hugging-face
+    """
+    assert version < 2
+    scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False,
+                              set_alpha_to_one=False)
+    model = StableDiffusionPipeline.from_pretrained(model_ids[version], use_auth_token=API_TOKEN, scheduler=scheduler,
+                                                    torch_dtype=torch.float32).to(device)
+    tokenizer = model.tokenizer
+    return model, tokenizer
+
+
+def get_controller(cfg, tokenizer, prompts, weight=None):
+    """
+    Get Prompt2Prompt controller
+    """
+    assert cfg.replace.apply is not cfg.refine.apply, print("Only one from replace/refine should be selected.")
+
+    local_blend = None
+    controller = None
+    if cfg.localize.apply:
+        local_blend = ptp_utils.LocalBlend(tokenizer, prompts, words=tuple(cfg.localize.words))
+
+    if cfg.replace.apply:
+        controller = ptp_utils.AttentionReplace(
+            tokenizer, prompts, cfg.steps, cfg.cross_replace_steps,
+            self_replace_steps=cfg.self_replace_steps, local_blend=local_blend
+        )
+    elif cfg.refine.apply:
+        controller = ptp_utils.AttentionRefine(
+            tokenizer, prompts, cfg.steps, cfg.cross_replace_steps,
+            self_replace_steps=cfg.self_replace_steps, local_blend=local_blend
+        )
+
+    if cfg.reweight.apply:
+        to_reweight = cfg.reweight.text
+        controller = reweight_controller(tokenizer, prompts, controller, local_blend, to_reweight, weight, cfg.steps)
+
+    return controller
+
+
+def replace_controller(tokenizer, prompts, lb=None, steps=50):
+    """
+    Generate controller for image replacement
+    """
+
+    controller = ptp_utils.AttentionReplace(
+        tokenizer, prompts, steps, cross_replace_steps={"default_": 1., "green": .4},
+        self_replace_steps=0.6, local_blend=lb
+    )
+
+    # controller = ptp_utils.AttentionReplace(
+    #     tokenizer, prompts, steps, cross_replace_steps={"default_": 1., "lasagne": .2},
+    #     cross_replace_steps=.8, self_replace_steps=0.4, local_blend=lb)
+    return controller
+
+
+def reweight_controller(tokenizer, prompts, controller, lb, to_reweight, weights, steps=50):
+    """
+    Generate controller for image reweight/refinement
+    """
+
+    # if weights = 10 and to_reweight="fried", we pay 10 times more attention to the word "fried"
+    equalizer = ptp_utils.get_equalizer(tokenizer, prompts[1], to_reweight, weights)
+    controller = ptp_utils.AttentionReweight(tokenizer, prompts, steps, cross_replace_steps=.8, self_replace_steps=.4,
+                                             equalizer=equalizer, local_blend=lb, controller=controller)
+    return controller
+
+
+def load_model_from_config(config_path="./configs/stable-diffusion/v2-inference-v.yaml",
+                           ckpt_path="./models/v2-1_768-ema-pruned.ckpt", verbose=False,
+                           return_tokenizer=False):
+    """
+    Load local pretrained model from config
+    """
+    config = OmegaConf.load(f"{config_path}")
+
+    print(f"Loading model from {ckpt_path}")
+    pl_sd = torch.load(ckpt_path, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    print(f"Model type: {type(model)}")
+    model.cuda()
+    model.eval()
+    model = model.to(device)
+    print(model)
+    if return_tokenizer:
+        tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+        return model, tokenizer
+
+    return model
